48a21b9f56ea5588f71a0b2fe8a4925aab81ee1b
rencho13 <stephone13@gmail.com>
2019-09-14 02:34:25 +0200

    Correction ok, test ok Object

--- src/exercices/ObjectTraining.java
+++ src/exercices/ObjectTraining.java
@@ -1,38 +1,48 @@
 public class ObjectTraining {
 
-    // TODO 1. Add the following private attributes :
-    // - name (String)
-    // - age (int)
-    // - wilder (boolean)
+
+		private String name;
+		private int age;
+		private boolean wilder;
 
 
-    // TODO 2. Add a Constructor with name and age arguments (in this order)
-    // This constructor must initialize name and age attributes
+		public ObjectTraining(String name, int age) {
+			this.name = name;
+			this.age = age;
+			
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public  int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		public boolean isWilder() {
+			return wilder;
+		}
+
+		public void setWilder(boolean wilder) {
+			this.wilder = wilder;
+		}
+
+		public String whoAmI() {
+			return "My name is " + this.getName() + " and I'm " + this.getAge(); 
+		}
 
 
-    // TODO 3. Add getter method for name attribute
+		public ObjectTraining() {
+		}
 
-
-    // TODO 4. Add setter method for name attribute
-
-
-    // TODO 5. Add getter method for age attribute
-
-
-    // TODO 6. Add setter method for age attribute
-
-
-    // TODO 7. Add getter method for wilder attribute
-
-
-    // TODO 8. Add setter method for wilder attribute
-
-
-    // TODO 9. Create a method 'whoAmI' that returns "My name is {name} and I'm {age}"
-    // where you replace {name} and {age} with attributes values
-
-
-    // Do not remove this empty constructor !
-    public ObjectTraining() {
-    }
+	
 }
